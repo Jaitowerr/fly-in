8e1d62e37f369a8483260ed49893a9e8cae4f7f3
Jaitowerr <207956068+Jaitowerr@users.noreply.github.com>
2026-06-04 11:39:51 +0200

    limpieza

--- fly_in.py
+++ fly_in.py
@@ -10,19 +10,12 @@ if __name__ == '__main__':
         parse.open_document()
 
         from src.start_program import Application
-        # from src import start_program as start
-        # from src import print_program as printp
+
         from src.print_program import Printer
 
         app = Application(parser=parse)
         app.printer = Printer()
         app.run()
-
-        # list_drones, list_hub, list_connect = start.list_object(arguments[0])
-
-        # printp.print_launch_drones(list_drones)
-
-        # start.start_program(list_drones, list_hub, list_connect)
 
     else:
         print("\033[31m", '\nEND OF PROGRAM - SEE YOU SOON!\n', "\033[1;33m")
